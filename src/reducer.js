42dc3624a8fa1a42983e04187410beb7fa9e7052
Justin Graber <jgraberdev@gmail.com>
2016-06-05 13:48:55 -0500

    Separate Vote State

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,14 @@
 import {setEntries, next, vote, INITIAL_STATE} from './core';
 
 export default function reducer(state = INITIAL_STATE, action) {
-    if (!action) { return state; }
-    switch(action.type) {
-        case 'SET_ENTRIES':
-          return setEntries(state, action.entries);
-        case 'NEXT':
-          return next(state);
-        case 'VOTE':
-          return vote(state, action.entry);
+    switch (action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries);
+    case 'NEXT':
+      return next(state);
+    case 'VOTE':
+      return state.update('vote',
+                          voteState => vote(voteState, action.entry));
     }
     return state;
 }
